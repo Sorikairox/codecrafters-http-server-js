59663ffc270368e9f516e1aa0d83b74321580b45
Sorikairo <sorikairox@gmail.com>
2023-12-20 22:37:01 +0900

    feat: respond HTTP/1.1 200 OK

--- app/main.js
+++ app/main.js
@@ -1,14 +1,13 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
     server.close();
   });
+
+  socket.write('HTTP/1.1 200 OK\\r\\n\\r\\n');
+  socket.pipe(socket);
 });
 
 server.listen(4221, "localhost");
