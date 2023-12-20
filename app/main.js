cdb488f96698c4e1f1b9b439787d0d057c9e4f0e
Sorikairo <sorikairox@gmail.com>
2023-12-20 22:52:48 +0900

    feat: respond HTTP/1.1 200 OK

--- app/main.js
+++ app/main.js
@@ -3,11 +3,19 @@ const net = require("net");
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
-    server.close();
+    // server.close();
   });
 
-  socket.write('HTTP/1.1 200 OK\\r\\n\\r\\n');
-  socket.pipe(socket);
+  socket.on('data', (data) => {
+    const text = data.toString();
+    const [info, ...others] = text.split('\n');
+    const [verb, path, version] = info.split(' ');
+    if (path === '/')
+      socket.write('HTTP/1.1 200 OK\\r\\n\\r\\n');
+    else
+      socket.write('HTTP/1.1 404 Not Found\\r\\n\\r\\n');
+    socket.destroySoon();
+  })
 });
 
 server.listen(4221, "localhost");
