4858f5057ec9a9287c96c5abe741cc5750d1c287
Sorikairo <sorikairox@gmail.com>
2023-12-20 23:01:29 +0900

    feat: add echo

--- app/main.js
+++ app/main.js
@@ -10,8 +10,16 @@ const server = net.createServer((socket) => {
     const text = data.toString();
     const [info, ...others] = text.split('\n');
     const [verb, path, version] = info.split(' ');
-    if (path === '/')
+    const pathComponents = path.split('/');
+    if (pathComponents.length === 0)
       socket.write('HTTP/1.1 200 OK\\r\\n\\r\\n');
+    else if (pathComponents[0] === 'echo') {
+      socket.write('HTTP/1.1 200 OK\\r\\n\\r\\n');
+      socket.write('Content-Type: text/plain\\r\\n\\r\\n');
+      socket.write(`Content-Length: ${pathComponents[1].length}: text/plain\\r\\n\\r\\n`);
+      socket.write('\\r\\n\\r\\n');
+      socket.write(`${pathComponents[1]}\\r\\n\\r\\n`);
+    }
     else
       socket.write('HTTP/1.1 404 Not Found\\r\\n\\r\\n');
   })
